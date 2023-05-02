1742c3a5ebf2998b3c2469ccf29f74700144fa91
Manuel <leyva4manuel@gmail.com>
2023-05-02 11:54:29 -0700

    updated signin and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './Components/SignUpComponent';
 import SignIn from './Components/SignInComponent';
 import CodewarsProfile from './Components/CodewarsProfileComponent/CodeWarsProfile';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './Components/SignUpComponent';
+import SignIn from './Components/SignInComponent';
 
 function App() {
   return (
