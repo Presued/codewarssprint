564dd7cb59269a24cc4057334a3b36269443f676
Brand0nLe <93brandonle@gmail.com>
2023-05-04 12:22:33 -0700

    Header and Footer gradients

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CodewarsProfile from './Components/CodewarsProfileComponent/CodeWarsProfile';
+import Header from './Components/HeaderComponent/Header';
+import Footer from './Components/FooterComponent/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <CodewarsProfile userId="Brand0nLe" />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/FooterComponent/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <div className="footer-gradient"></div>
+    );
+};
+
+export default Footer;
