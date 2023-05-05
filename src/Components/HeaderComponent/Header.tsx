24971e30e8815319935aea12ee87c08500e34946
Brand0nLe <93brandonle@gmail.com>
2023-05-05 09:42:26 -0700

    added logout button

--- src/Components/HeaderComponent/Header.tsx
+++ src/Components/HeaderComponent/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/images/CodeReserve.png';
+import logout from '../../assets/images/LogOut.png'
 
 const Header: React.FC = () => {
     return (
         <div className="header">
             <img src={logo} alt="logo" className="header-logo" />
+            <img src={logout} alt="logout button" className="logout-button" />
             <div className="header-gradient"></div>
         </div>
     );
