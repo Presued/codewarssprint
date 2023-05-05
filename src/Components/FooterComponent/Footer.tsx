ba7ef7cd5cfe591cfd3bdd98afc6f0c926cf7dd1
Brand0nLe <93brandonle@gmail.com>
2023-05-05 09:01:54 -0700

    Trying to edit footer with MUI icons

--- src/Components/FooterComponent/Footer.tsx
+++ src/Components/FooterComponent/Footer.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Footer.css';
-// import GitHubIcon from '@material-ui/icons/GitHub';
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 const Footer: React.FC = () => {
     return (
-        <div className="footer-gradient"></div>
+        <div className="footer-gradient">
+            <div className="left">
+                © 2023 Your Company Name
+            </div>
+            <div className="right">
+                <GitHubIcon />
+                <LinkedInIcon />
+                <TwitterIcon />
+            </div>
+        </div>
     );
 };
 
 export default Footer;
+
